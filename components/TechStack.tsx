1d5a6d6a50d8ded92bb86560dcba180536420507
cjabendan <cjamesabendan@gmail.com>
2026-05-29 11:30:20 +0800

    feat: design tech list

--- components/TechStack.tsx
+++ components/TechStack.tsx
@@ -3,9 +3,16 @@ export default function TechStack() {
     <div className="bento-card p-4 col-span-1 md:col-span-4 space-y-2 group animate-fade-in animation-delay-200">
       <div className="flex items-center justify-between">
         <h2 className="text-lg font-bold">Tech Stack</h2>
-        <a className="text-xs text-foreground/70 hover:text-foreground flex items-center gap-1 transition-colors"  href="">View All</a>
+        <a
+          className="text-xs text-foreground/70 hover:text-foreground flex items-center gap-1 transition-colors"
+          href=""
+        >
+          View All
+        </a>
       </div>
-     
+      <div>
+        List
+      </div>
     </div>
   );
 }
